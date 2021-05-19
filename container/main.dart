22748679cf37048277594727db066a56ed80234e
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2021-05-19 16:52:43 -0300

    main.dart

---
+++ container/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Título do app",
+    home: Container(
+      //Container- Container que inclui outro widget.
+      /*
+      Container(
+      {Key? key,
+        AlignmentGeometry? alignment,
+        EdgeInsetsGeometry? padding,
+        Color? color,
+        Decoration? decoration,
+        Decoration? foregroundDecoration,
+        double? width,
+        double? height,
+        BoxConstraints? constraints,
+        EdgeInsetsGeometry? margin,
+        Matrix4? transform,
+        AlignmentGeometry? transformAlignment,
+        Widget? child,
+        Clip clipBehavior = Clip.none}
+        )
+       */
+      color: Colors.white,
+    ),
+  ));
+}
